219d7d3f4b7daa6958fd7277c5a641830fa659fa
esam.alsawah2000 <esam.alsawah2000@gmail.com>
2024-01-02 20:55:01 +0300

    Fix : padding for App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import ThemeProvider from '@/theme/ThemeProvider'
 import Leaflet from '@/containers/LeafletContainer'
+import { Box } from '@mui/material'
 const App = () => {
   return (
     <ThemeProvider>
-      <Leaflet />
+      <Box sx={{ p: '2%' }}>
+        <Leaflet />
+      </Box>
     </ThemeProvider>
   )
 }
